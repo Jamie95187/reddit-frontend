4909f11d2c350ea416033d19117a56eb45cedc2f
Jamie <jamiewong63@googlemail.com>
2021-06-03 23:18:51 +0100

    render chakra form control component for register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { FormControl, FormLabel, Input, FormErrorMessage } from "@chakra-ui/react";
 
 interface registerProps {
 
 }
 
-export const register: React.FC<registerProps> = ({}) => {
-  return();
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+    {({values, handleChange}) => (
+      <Form>
+        <FormControl>
+          <FormLabel htmlFor="username">Username</FormLabel>
+          <Input
+            value={values.username}
+            onChange={handleChange}
+            id="username"
+            placeholder="name" />
+           {/*<FormErrorMessage>{form.errors.name}</FormErrorMessage>*/}
+        </FormControl>
+      </Form>
+    )}
+    </Formik>
+  );
 }
+
+export default Register;
