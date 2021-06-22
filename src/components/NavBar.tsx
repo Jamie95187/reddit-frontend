bb6901fbfa0b17a96cc589f60848035a3dd71ea9
Jamie <jamiewong63@googlemail.com>
2021-06-22 05:07:03 +0100

    add links to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Link, Flex } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 interface NavBarProps {
 
@@ -7,6 +8,15 @@ interface NavBarProps {
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   return (
-    <Box bg='salmon'>Henlo</Box>
+    <Flex bg='salmon' p={4} ml={"auto"}>
+      <Box ml={"auto"}>
+      <NextLink href="/login">
+        <Link colour='white' mr={2}> login </Link>
+      </NextLink>
+      <NextLink href="/register">
+        <Link colour='white'> register </Link>
+      </NextLink>
+      </Box>
+    </Flex>
   )
 }
