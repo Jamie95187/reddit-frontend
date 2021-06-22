588fcf5bfa101649c5733f68fc1aaeca8a81eb49
Jamie <jamiewong63@googlemail.com>
2021-06-22 05:01:23 +0100

    create navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+interface NavBarProps {
+
+}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Box bg='salmon'>Henlo</Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
-const Index = () => <div>hello world</div>;
+import { NavBar } from '../components/NavBar';
 
+const Index = () =>
+  <>
+    <NavBar />
+    <div>hello world</div>
+  </>
 export default Index
