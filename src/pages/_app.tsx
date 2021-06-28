90b88b6f3e2c02d6750f73eb0abeb0a53bad87eb
Jamie <jamiewong63@googlemail.com>
2021-06-28 22:01:59 +0100

    no errors after restarting docker and servers. Also displays username on navbar when user is logged in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,57 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { cacheExchange, Cache } from '@urql/exchange-graphcache';
 import theme from '../theme'
 import { MeDocument } from "../generated/graphql";
+
+// Allow us to properly cast types
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:8080/graphql',
   exchanges: [dedupExchange, cacheExchange({
     updates: {
       Mutation: {
-        login: (result, args, cache, info) => {
-          cache.updateQuery({query: MeDocument})
-        }
-      }
-    }
+        login: (_result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery> (cache,
+              {query: MeDocument},
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+          );
+        },
+
+        createUser: (_result, args, cache, info) => {
+          betterUpdateQuery<CreateUserMutation, MeQuery> (cache,
+              {query: MeDocument},
+              result,
+              (result, query) => {
+                if (result.createUser.errors) {
+                  return query
+                } else {
+                  return {
+                    me: result.createUser.user,
+                  };
+                }
+              }
+          );
+        },
+      },
+    },
   }), fetchExchange]
 });
 
